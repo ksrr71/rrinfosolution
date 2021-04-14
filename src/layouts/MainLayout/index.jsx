c6d0313b71ab52568a9cf1169622bb5a2149e164
Himanshu Tariyal <tariyal2000@gmail.com>
2021-04-14 14:49:58 +0530

    upate

--- src/layouts/MainLayout/index.jsx
+++ src/layouts/MainLayout/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Head from "next/head";
 
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
@@ -8,6 +8,14 @@ import styles from "./styles.module.scss";
 export default function index({ children }) {
   return (
     <>
+      <Head>
+        <title>ReachTheWeb</title>
+        <meta
+          name="description"
+          content="ReachTheWeb is a development studio which equips creative founders with on-demand design and development team to execute their vision into reality. Our services include Dyamic Web Applications, Cross-platform Mobile Applications, custom made servers and much more."
+        />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+      </Head>
       <Navbar />
       <div className={styles.wrapper}>
         <div className={styles.container}>{children}</div>
